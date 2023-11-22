7a446dff5e6f2c01bb533039fe50484f8fd47637
Marsel Shakirov <imanager666@gmail.com>
2023-11-22 23:15:26 +0300

    add/removeItem-reducer

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -2,15 +2,23 @@ import React from "react";
 
 import type { ITask } from "../../types/toDo";
 
-import styles from "./Task.module.scss";
-import { IoClose } from "react-icons/io5";
+import { useAppDispatch } from "../../hooks/redux.hooks";
+import { removeItem } from "../../redux/slices/toDoSlice";
 
-const Task: React.FC<ITask> = ({ title }) => {
+import { IoClose } from "react-icons/io5";
+import styles from "./Task.module.scss";
+
+const Task: React.FC<ITask> = ({ id, title }) => {
+  const dispatch = useAppDispatch();
+
   return (
     <li className={styles.root}>
       <input className={styles.checkbox} type="checkbox" name="task" />
       <label className={styles.title}>{title}</label>
-      <IoClose className={styles.close} />
+      <IoClose
+        onClick={() => dispatch(removeItem(id))}
+        className={styles.close}
+      />
     </li>
   );
 };
--- src/redux/slices/toDoSlice.ts
+++ src/redux/slices/toDoSlice.ts
@@ -5,7 +5,7 @@ import type { ITaskItems } from "../../types/toDo";
 import type { ITask } from "../../types/toDo";
 
 const initialState: ITaskItems = {
-  items: [{ id: "", title: "Marsel" }],
+  items: [{ id: "12", title: "Marsel" }],
 };
 
 export const toDoSlice = createSlice({
@@ -15,8 +15,12 @@ export const toDoSlice = createSlice({
     addItem: (state, action: PayloadAction<ITask>) => {
       state.items.push(action.payload);
     },
+    removeItem: (state, action: PayloadAction<string>) => {
+      console.log(action.payload);
+      state.items = state.items.filter((item) => item.id !== action.payload);
+    },
   },
 });
 
-export const { addItem } = toDoSlice.actions;
+export const { addItem, removeItem } = toDoSlice.actions;
 export default toDoSlice.reducer;
